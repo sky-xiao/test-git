4a47dce05e13cdb6d91d2274221864567d9d1416
sky-xiao <15574814866@163.com>
2019-08-25 12:59:51 +0800

    First Commit

---
+++ src/main/java/com/example/testgit/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.example.testgit.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author xiaoky
+ * @date 2019-08-25
+ */
+@RestController
+public class TestController {
+
+    /**
+     * 测试方法
+     * @return String
+     */
+    @GetMapping(value = "/test")
+    public  String test() {
+        return "Hello world";
+    }
+}
